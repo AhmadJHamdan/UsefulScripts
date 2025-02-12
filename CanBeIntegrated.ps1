ae802a70da7879b68e60809372d6c94870c48893
AhmadJHamdan <102372344+AhmadJHamdan@users.noreply.github.com>
2025-02-12 11:34:35 +0200

    Update CanBeIntegrated.ps1

--- CanBeIntegrated.ps1
+++ CanBeIntegrated.ps1
@@ -1,6 +1,7 @@
 foreach($i in $(Get-Content .\IPs.txt)){
 try{
-Get-EventLog -Newest 10 -ComputerName $i -LogName Security;
+# Get-EventLog -Newest 10 -ComputerName $i -LogName Security;
+Get-WinEvent -ComputerName $i -LogName Security -MaxEvents 10;
 $i >> .\worked.txt
 }catch{
 $i >> .\didnotwork.txt;
